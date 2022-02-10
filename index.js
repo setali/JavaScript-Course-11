7bd0e2542eff83913f0f464175d803163fc19de0
salimousavi <set.ali.mousavi@gmail.com>
2022-02-10 20:29:54 +0330

    Session 1

---
+++ index.js
@@ -0,0 +1,57 @@
+// var b = 'ali'
+// var a = 12 - b
+
+// It's wrong
+// if (a === NaN) {
+//     // Code
+// }
+
+// var var1 = 10,
+//   var2 = 'Ali',
+//   var3 = true
+
+// console.log(typeof var1)
+// console.log(typeof(var2))
+// console.log(typeof (var3))
+
+// var myVar = 'Ali'
+
+// console.log(myVar)
+
+// myVar = 52
+
+// console.log(myVar)
+
+// var نام = 'علی'
+
+// var 1myName = 'Ali' // Worng
+// var @myname
+
+// var age = 32
+
+// var Age = 42
+
+// var agE = 50
+
+// var myName = 'Ali'
+// var myFamily = 'Mousavi'
+// var age = 32
+// var gender = true
+
+// console.log(age)
+
+// console.log('ali')
+// console.error('Eli')
+
+// console.table({ name: 'Ali', family: 'Mousavi' })
+
+// This is a sample for sync and async
+/*function order () {
+  logToDB()
+  sendSms()
+  renderPage()
+}*/
+
+// const BASE_URL = "http://mysite.com" // This is My Base url
+
+// const COOKIE_TIME = 60 // In second
