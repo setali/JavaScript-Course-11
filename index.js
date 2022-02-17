5d495b12e25a5fb59eb2b0f7848951fffc132581
salimousavi <set.ali.mousavi@gmail.com>
2022-02-17 19:45:50 +0330

    Session 2

--- index.js
+++ index.js
@@ -1,57 +1,144 @@
-// var b = 'ali'
-// var a = 12 - b
+var a = 21
 
-// It's wrong
-// if (a === NaN) {
-//     // Code
+a % 2 ? console.log('Odd') : console.log('Even')
+
+console.log(a % 2 ? 'Odd' : 'Even')
+
+// var a = 14
+
+// var state = a % 2 === 1 ? 'Odd' : 'Even'
+
+// console.log('a is ' + state)
+
+// var char = 'C'
+
+// switch (char) {
+//   case 'a':
+//   case 'A':
+//     console.log('Char is a')
+//     break
+
+//   case 'b':
+//   case 'B':
+//     console.log('Char is b')
+//     break
+
+//   default:
+//     console.log('Char is not a or b')
 // }
 
-// var var1 = 10,
-//   var2 = 'Ali',
-//   var3 = true
+// var a = 0
 
-// console.log(typeof var1)
-// console.log(typeof(var2))
-// console.log(typeof (var3))
+// if (a > 0) {
+//   console.log('a is positive')
+// } else if (a < 0) {
+//   console.log('a is negative')
+// } else {
+//   console.log(' a is zero')
+// }
 
-// var myVar = 'Ali'
+// var a = 17
 
-// console.log(myVar)
+// if (a % 2) {
+//   console.log('a is odd')
+// } else {
+//   console.log('a is even')
+// }
 
-// myVar = 52
+// var person = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi',
+//   fullName: 'SAli Mousavi'
+// }
 
-// console.log(myVar)
+// var fullName = person.fullName || person.firstName + ' ' + person.lastName
 
-// var نام = 'علی'
+// console.log(fullName)
 
-// var 1myName = 'Ali' // Worng
-// var @myname
+// 0 & 0 => 0
+// 0 & 1 => 0
+// 1 & 0 => 0
+// 1 & 1 => 1
 
-// var age = 32
+// 0 | 0 => 0
+// 1 | 0 => 1
+// 0 | 1 => 1
+// 1 | 1 => 1
 
-// var Age = 42
+// var a = '0'
 
-// var agE = 50
+// console.log(0 === +a)
+
+// console.log('b' + 'a' + NaN + 'a')
+
+// console.log('Ali'.toUpperCase())
+
+// var friends = ['Ali', 'Eli', 'Qoli', 'Fati', 4312, true, {}]
+
+// console.log(typeof friends)
+
+// console.log(friends.length)
+
+// console.log(friends[4])
+
+// friends[0] = 'Hasan'
+// console.log(friends)
+
+// EA12F3
+
+// var person = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi',
+//   fullName: 'SAli Mousavi',
+//   age: 32,
+//   weight: 82,
+//   family: {
+//     father: 'Mohammad',
+//     mother: 'Hajar',
+//     sisters: ['Fati', 'Zari', 'Zeinab'],
+//     brother: 'Mostafa',
+//     wife: 'Eli',
+//     daughters: ['Mahsa']
+//   }
+// }
+
+// console.log(typeof person)
+
+// var propertyName
+
+// if (person.age > 20) {
+//   propertyName = 'fullName'
+// } else {
+//   propertyName = 'firstName'
+// }
+
+// console.log(person[propertyName])
+
+// console.log(person['fullName'])
+
+// console.log(person[propertyName])
+
+// console.log(person['firstName'])
+// console.log(person.firstName)
+
+// var person2 = person
+
+// console.log(person2)
+
+// person.firstName = 'Eli'
+
+// console.log(person2)
+
+// console.log(person === person2)
+
+// console.log(person.firstName)
+// console.log(person.lastName)
+// console.log(person.family.father)
 
 // var myName = 'Ali'
 // var myFamily = 'Mousavi'
-// var age = 32
-// var gender = true
+// var myAge = 32
 
-// console.log(age)
+// var str = 'My name is \u{1F600} Ali'
 
-// console.log('ali')
-// console.error('Eli')
-
-// console.table({ name: 'Ali', family: 'Mousavi' })
-
-// This is a sample for sync and async
-/*function order () {
-  logToDB()
-  sendSms()
-  renderPage()
-}*/
-
-// const BASE_URL = "http://mysite.com" // This is My Base url
-
-// const COOKIE_TIME = 60 // In second
+// console.log(str)
